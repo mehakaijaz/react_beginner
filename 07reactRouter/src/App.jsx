060e0fa799cc3423530a5ae2cfae3d1c5baa8090
mehak aijaz <mehakaijaz716@gmail.com>
2024-07-15 12:15:12 +0530

    react routers

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './components/Home'
+
+function App() {
+  
+  return (
+    <>
+      <h1>react routing</h1>
+      <Header/>
+      <Home/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
